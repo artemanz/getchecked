1493f498a3334db07557f2601a0a225e62cb5295
Artem Anzin <anzinartix@gmail.com>
2024-06-27 10:23:52 +0300

    add google tag function to whatsapp

--- src/components/contact-menu/index.tsx
+++ src/components/contact-menu/index.tsx
@@ -2,6 +2,14 @@ import { FaPhoneAlt } from "react-icons/fa"
 import { FaWhatsapp } from "react-icons/fa"
 
 export const ContactMenu = () => {
+  const handleWhatsAppClick = () => {
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+      event: "whatsapp_conversion_click",
+      conversionValue: 1, // set this if you want to pass a specific value
+    })
+  }
+
   return (
     <div className="fixed right-4 bottom-4 flex flex-col gap-4 z-10">
       <a
@@ -12,6 +20,7 @@ export const ContactMenu = () => {
       </a>
 
       <a
+        onClick={handleWhatsAppClick}
         className="bg-green-400 w-16 aspect-square grid place-content-center rounded-full"
         href="https://wa.me/97143331473"
         target="_blank"
